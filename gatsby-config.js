5fa70b104c32830b824ec071cc5805a59bf6b32e
pubik <pubik2021@gmail.com>
2021-04-24 16:01:29 +0200

    added new tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
           resolve: `gatsby-plugin-google-analytics`,
           options: {
               // The property ID; the tracking code won't be generated without it
-              trackingId: "G-QXFXVWS8GT",
+              trackingId: "UA-195356618-2",
               // Defines where to place the tracking script - `true` in the head and `false` in the body
               head: true,
               // Setting this parameter is optional
